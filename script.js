4248599c7fa0e72e2e84597fd72af24f896a7e67
Anchal Patel <141991791+anchalPatel03@users.noreply.github.com>
2025-04-14 16:47:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// static/js/script.js
+
+document.getElementById('sensorForm').addEventListener('submit', function(event) {
+    // Show loading spinner when form is submitted
+    document.getElementById('loadingSpinner').style.display = 'block';
+});
